30bec3ca854a0d489bea888816b44afe073e9931
wi21b067 <wi21b067@technikum-wien.at>
2022-05-19 20:32:47 +0200

    Initial commit

---
+++ src/main/java/com/example/sumcalculator/SumCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sumcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sumcalculator/SumCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
